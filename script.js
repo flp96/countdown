e4df88bcfe7f86e31aa2da23cc3bddb257da9427
flp96 <filipefernandes__@live.com>
2024-07-01 13:55:06 -0300

    mostrando contagem na tela

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ import Countdown from './countdown.js';
 const tempoParaONatal = new Countdown('24 December 2024 23:59:59 GMT-0300');
 const tempoParaOAnoNovo = new Countdown('31 December 2024 23:59:59 GMT-0300');
 
+const tempoNatal = document.querySelector('.tempo-natal');
+const tempoAnoNovo = document.querySelector('.tempo-ano-novo');
 
-console.log(tempoParaONatal.total);
+
 setInterval(() => {
-  console.log(tempoParaOAnoNovo.total);
-}, 1000)
-
+tempoNatal.innerHTML = tempoParaONatal.days + ' dias, ' + tempoParaONatal.hours + ' horas, ' + tempoParaONatal.minutes + ' minutos e ' + tempoParaONatal.seconds + ' segundos ' + 'para o Natal';
+tempoAnoNovo.innerHTML = tempoParaOAnoNovo.days + ' dias, ' + tempoParaOAnoNovo.hours + ' horas, ' + tempoParaOAnoNovo.minutes + ' minutos e ' + tempoParaOAnoNovo.seconds + ' segundos ' + 'para o Ano Novo';
+}, 1000);
 
